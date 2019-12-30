628b5bbdf4ea6589375ebc6a12f3c0d067be7ec2
Sreekiran AR <sreekiran.ar@bridgei2i.com>
2019-12-30 12:31:02 +0530

    now works with paths like /media/user/New Volume/filePath also

--- MakeTreeDir/makedir.py
+++ MakeTreeDir/makedir.py
@@ -5,26 +5,31 @@ class MAKETREEDIR:
 		pass
 	def makedir(self,dir_path):
 		"""Short summary.
-
 		Args:
-			dir_path (type): Path of the directory to be created ( can be subfolder also).
-
+			dir_path (type): Path of the directory to be created (can be subfolder also).
 		Returns:
 			type: 0 if success
-
 		Examples
 			Examples should be written in doctest format, and
 			should illustrate how to use the function/class.
 			>>> from MakeTreeDir import MAKETREEDIR
-			>>> MAKETREEDIR().makedir('this/is/a/testing/package/creation')
-
+			>>> md=MAKETREEDIR()
+			>>> md.makedir('this/is/a/testing/package/creation')
+			# Can also deal with inputs like
+			>>> md.makedir('/media/username/New Volume/folderName')
+			>>> md.makedir('../../../folderName')
 		"""
-		sep='/' if '/' in dir_path else '\\'
-		dirs=dir_path.split(sep)
+		try:
+			sep='/' if '/' in dir_path else '\\'
+			dirs=dir_path.split(sep)
+			if len(dirs[0])==0:
+				dirs[1]=sep+dirs[1]
+			dirs=dirs[1:]
+			for i,fol in enumerate(dirs):
+				path=dirs[:i+1]
+				directory=os.path.join(*path)
+				os.makedirs(directory,exist_ok=True)
+		except Exception as e:
+			return e
 
-		for i,fol in enumerate(dirs):
-			path=dirs[:i+1]
-			directory=os.path.join(*path)
-			if not os.path.exists(directory):
-				os.makedirs(directory)
 		return 0
