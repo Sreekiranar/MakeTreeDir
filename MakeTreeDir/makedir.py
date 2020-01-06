b67195863c01b26eadfa254fcdaba11950f49a61
Sreekiran AR <sreekiran.ar@bridgei2i.com>
2020-01-06 12:05:06 +0530

    made compatible with windows also

--- MakeTreeDir/makedir.py
+++ MakeTreeDir/makedir.py
@@ -18,17 +18,28 @@ class MAKETREEDIR:
 			# Can also deal with inputs like
 			>>> md.makedir('/media/username/New Volume/folderName')
 			>>> md.makedir('../../../folderName')
+			>>> md.makedir('C://Users/userName/folderName')
 		"""
 		try:
 			sep='/' if '/' in dir_path else '\\'
 			dirs=dir_path.split(sep)
+
+			### for linux abs path
 			if len(dirs[0])==0:
 				dirs[1]=sep+dirs[1]
 				dirs=dirs[1:]
+
+			### for windows abs path
+			elif  ':' in dirs[0]:
+				dirs[1]=dirs[0]+sep+dirs[1]
+				dirs=dirs[1:]
+
+			### making directory
 			for i,fol in enumerate(dirs):
 				path=dirs[:i+1]
 				directory=os.path.join(*path)
 				os.makedirs(directory,exist_ok=True)
+				
 		except Exception as e:
 			return e
 
